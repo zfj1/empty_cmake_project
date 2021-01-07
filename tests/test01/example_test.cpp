10518066bf892b571f3a654f38a8aafefad1b757
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-01-07 17:30:37 -0600

    just adding a couple more tests

--- tests/test01/example_test.cpp
+++ tests/test01/example_test.cpp
@@ -1,10 +1,23 @@
 #include "example.h"
+
 #include "gtest/gtest.h"
 
 TEST(ExampleTest, HandlesAgtB) {  // uses the GoogleTest framework
     int A = 30, B = 5, C;
     C = DoExampleCalculation(A, B);
     EXPECT_EQ(C, 2 * A - B);
+}
+
+TEST(ExampleTest, HandlesAeqB) {
+    int A = 5, B = 5, C;
+    C = DoExampleCalculation(A, B);
+    EXPECT_EQ(C, 0);
+}
+
+TEST(ExampleTest, HandlesAltB) {
+    int A = 3, B = 7, C;
+    C = DoExampleCalculation(A, B);
+    EXPECT_EQ(C, 597);
 }
 
 int main(int argc, char* argv[]) {
